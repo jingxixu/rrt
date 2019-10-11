de93a30c1b63a221f8f3512bb0dddfd66e72c7f6
Jingxi Xu <zealliant@outlook.com>
2019-10-11 00:02:21 -0400

    initial

---
+++ README.md
@@ -0,0 +1,5 @@
+# Lab 3 - Rapidly-exploring Random Tree (RRT)
+Lab 3 for [COMSW4733 Computational Aspects of Robotics](https://www.cs.columbia.edu/~allen/F19/) at Columbia University (Instructor: [Prof. Peter Allen](http://www.cs.columbia.edu/~allen/)).
+
+## Introduction
+In this lab, you are required to implement the Rapidly-exploring Random Tree (RRT) algorithm for motion planning on a 3DOF robotic arm.
---
+++ demo.py
@@ -0,0 +1,106 @@
+from __future__ import division
+import pybullet as p
+import pybullet_data
+import numpy as np
+import time
+import argparse
+
+
+UR5_JOINT_INDICES = [0, 1, 2]
+
+
+def set_joint_position(body, joint, value):
+    p.resetJointState(body, joint, value)
+
+
+def set_joint_positions(body, joints, values):
+    assert len(joints) == len(values)
+    for joint, value in zip(joints, values):
+        set_joint_position(body, joint, value)
+
+
+def draw_sphere_marker(position, radius, color):
+   vs_id = p.createVisualShape(p.GEOM_SPHERE, radius=radius, rgbaColor=color)
+   marker_id = p.createMultiBody(basePosition=position, baseCollisionShapeIndex=-1, baseVisualShapeIndex=vs_id)
+   return marker_id
+
+
+def remove_marker(marker_id):
+   p.removeBody(marker_id)
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Run Dynamic Grasping Experiment')
+    parser.add_argument('--birrt', action='store_true', default=False)
+    args = parser.parse_args()
+    return args
+
+
+def rrt():
+    ###############################################
+    # TODO your code to implement the rrt algorithm
+    ###############################################
+    pass
+
+
+def birrt():
+    #################################################
+    # TODO your code to implement the birrt algorithm
+    #################################################
+    pass
+
+
+if __name__ == "__main__":
+    args = get_args()
+
+    # set up simulator
+    physicsClient = p.connect(p.GUI)
+    p.setAdditionalSearchPath(pybullet_data.getDataPath())
+    p.setPhysicsEngineParameter(enableFileCaching=0)
+    p.setGravity(0, 0, -9.8)
+    p.configureDebugVisualizer(p.COV_ENABLE_GUI, False)
+    p.configureDebugVisualizer(p.COV_ENABLE_SHADOWS, True)
+    p.resetDebugVisualizerCamera(cameraDistance=1.400, cameraYaw=58.000, cameraPitch=-42.200, cameraTargetPosition=(0.0, 0.0, 0.0))
+
+    # load objects
+    plane = p.loadURDF("plane.urdf")
+    ur5 = p.loadURDF('assets/ur5/ur5.urdf', basePosition=[0, 0, 0.02], useFixedBase=True)
+    obstacle1 = p.loadURDF('assets/block.urdf',
+                           basePosition=[1/4, 0, 1/2],
+                           useFixedBase=True)
+    obstacle2 = p.loadURDF('assets/block.urdf',
+                           basePosition=[2/4, 0, 2/3],
+                           useFixedBase=True)
+    obstacles = [plane, obstacle1, obstacle2]
+
+    ## start and goal
+    start_conf = (-0.813358794499552, -0.37120422397572495, -0.754454729356351)
+    start_position = (0.3998897969722748, -0.3993956744670868, 0.6173484325408936)
+    goal_conf = (0.7527214782907734, -0.6521867735052328, -0.4949270744967443)
+    goal_position = (0.35317009687423706, 0.35294029116630554, 0.7246701717376709)
+    goal_marker = draw_sphere_marker(position=goal_position, radius=0.02, color=[1, 0, 0, 1])
+    set_joint_positions(ur5, UR5_JOINT_INDICES, start_conf)
+
+    # place holder to save the solution path
+    path_conf = None
+
+    if not args.birrt:
+        # using rrt
+        path_conf = rrt()
+    else:
+        # using birrt
+        path_conf = birrt()
+
+    if path_conf is None:
+        # pause here
+        raw_input("no collision-free path is found within the time budget, finish?")
+    else:
+        ###############################################
+        # TODO your code to highlight the solution path
+        ###############################################
+
+        # execute the path
+        while True:
+            for q in path_conf:
+                set_joint_positions(ur5, UR5_JOINT_INDICES, q)
+                time.sleep(0.5)
